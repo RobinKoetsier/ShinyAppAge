836a081626f8298bbc5ab5df742c55851365a32d
RobinWilhelmus <45625446+RobinWilhelmus@users.noreply.github.com>
2020-06-19 15:32:06 +0200

    Add files via upload

---
+++ Helpers.R
@@ -0,0 +1,152 @@
+TransfermarktShiny<- function(team_name, team_num) {
+  
+  session <- bow(glue::glue("https://www.transfermarkt.com/{team_name}/leistungsdaten/verein/{team_num}/plus/1?reldata=%26{season}/"))
+  #session <- bow(url)
+  # grab name from photo element instead
+  result_name <- scrape(session) %>% 
+    html_nodes("#yw1 .bilderrahmen-fixed") %>% 
+    html_attr("title") 
+  
+  # grab age
+  result_age <- scrape(session) %>% 
+    html_nodes(".posrela+ .zentriert") %>% 
+    html_text()
+  
+  # grab minutes played in league
+  result_mins <- scrape(session) %>% 
+    html_nodes("td.rechts") %>% 
+    html_text()
+  
+  ## get length
+  session <- bow(glue::glue("https://www.transfermarkt.com/{team_name}/kader/verein/{team_num}/saison_id/{season}/plus/1"))
+  
+  result_name2 <- scrape(session) %>% 
+    html_nodes("#yw1 .bilderrahmen-fixed") %>% 
+    html_attr("title") 
+  
+  result_bday <- scrape(session) %>% 
+    html_nodes(".posrela+ .zentriert") %>% 
+    html_text()
+  
+  result_joinedteam <- scrape(session) %>% 
+    html_nodes("td:nth-child(7)") %>% 
+    html_text()
+  
+  result_leaveteam <- scrape(session) %>% 
+    html_nodes("td:nth-child(9)") %>% 
+    html_text()
+  
+  # place each vector into list
+  resultados <- list(result_name, result_age, result_mins)
+  
+  col_name <- c("name", "age", "minutes")
+  
+  results_comb <- resultados %>% 
+    reduce(cbind) %>% 
+    as_tibble() %>% 
+    set_names(col_name)
+  
+  ## join + bday
+  resultados2 <- list(result_name2, result_bday, 
+                      result_joinedteam, result_leaveteam)
+  
+  col_name2 <- c("name", "bday", "join", "leave")
+  
+  results_comb2 <- resultados2 %>% 
+    reduce(cbind) %>% 
+    as_tibble() %>% 
+    set_names(col_name2)
+  
+  ## combine BOTH
+  results_comb <- results_comb %>% 
+    left_join(results_comb2) 
+  all_team_minutes$age2 <- (Sys.Date() - all_team_minutes$bday)/365.25
+  
+  session <- bow(
+    glue::glue(
+      "https://www.transfermarkt.com/{team_name}/kadernaechstesaison/verein/{team_num}/geruechte/0/anschluss/0/plus/1"))
+  
+  result_name3 <- scrape(session) %>% 
+    html_nodes("#yw1 .bilderrahmen-fixed") %>% 
+    html_attr("title") 
+  
+  result_bday2 <- scrape(session) %>% 
+    html_nodes(".posrela+ .zentriert") %>% 
+    html_text()
+  
+  result_joinedteam2 <- scrape(session) %>% 
+    html_nodes("td:nth-child(5)") %>% 
+    html_text()
+  
+  result_leaveteam2 <- scrape(session) %>% 
+    html_nodes("td:nth-child(7)") %>% 
+    html_text()
+  print("last joun test")
+  # place each vector into list
+  resultados3 <- list(result_name3, result_bday2, 
+                      result_joinedteam2, result_leaveteam2)
+  
+  col_name3 <- c("name", "bday2", "join2", "leave2")
+  
+  results_comb3 <- resultados3 %>% 
+    reduce(cbind) %>% 
+    as_tibble() %>% 
+    set_names(col_name3)
+  
+  results_comb <- results_comb %>% 
+    left_join(results_comb3) 
+  results_comb$bday <-   ifelse(is.na(results_comb$bday) ,results_comb$bday2,results_comb$bday)
+  results_comb$join <-   ifelse(is.na(results_comb$join) ,results_comb$join2,results_comb$join)
+  results_comb$leave <-   ifelse(is.na(results_comb$leave) ,results_comb$leave2,results_comb$leave)
+  results_comb <- select(results_comb,1,2,3,4,5,6)
+  # fix "strings" into proper formats, calculate % of minutes appeared
+  all_team_minutes <- results_comb %>% 
+    mutate(age = as.numeric(age),
+           minutes = minutes %>% 
+             str_replace("\\.", "") %>% 
+             str_replace("'", "") %>% 
+             as.numeric(),
+           min_perc = (minutes / total_minutes) %>% round(digits = 3),
+           bday = str_replace_all(bday, "\\(.*\\)", "") %>% mdy(),
+           join = join %>% mdy(),
+           join_age = interval(bday, join) / years(1),
+           leave = leave %>% dmy(),
+           leave_age = interval(bday, leave) / years(1),
+           age_now = interval(bday, Sys.Date()) / years(1)) %>% 
+    filter(!is.na(minutes)) 
+  all_team_minutes$name <- all_team_minutes$name %>% str_replace_all("^(\\w)\\w+ (?=\\w)", "\\1.")
+  
+  return(all_team_minutes)
+}
+
+ScatterShiny <- function(data,color1,color2,color3,teamname,alpha){
+  #data<- all_team_minutes
+  teamname <- gsub("-"," ",teamname)
+  
+  ggplot(data, aes(x=age_now, y=minutes)) +
+    geom_rect(aes(xmin=25,xmax=30, ymin=-Inf,ymax= Inf), fill = color1, alpha=0.01 )+
+   
+    ggrepel::geom_text_repel(aes(label = name, family = "Spartan-Light"), size = 3) +
+    ggforce::geom_link(aes(x=age_now, xend=leave_age, y = minutes, yend = minutes,alpha = (1*-stat(index))), color=color3) +
+    ggforce::geom_link(aes(x=age_now, xend=join_age, y = minutes, yend = minutes, alpha = -stat(index)),color=color2)+
+    geom_point(color = "black", size = 2) +
+    theme_bw() + 
+     aes(ymin=0) +
+    scale_x_continuous(breaks = pretty_breaks(n = 10)) +
+    labs(x = paste("Age on",format(Sys.time(), "%d %b %Y")),
+         y = "Minutes played",
+         title = paste("Age plot", teamname)) +
+    theme(
+      text = element_text(family = "Spartan-Light"),
+      plot.title = element_text(size = 15, hjust = 0.5),
+      plot.subtitle = element_text(size = 10),
+      plot.caption = element_text(size = 12),
+      axis.title = element_text(size = 10),
+      axis.text = element_text(size = 10),
+      panel.grid.minor.x = element_blank(),
+      legend.position = "none")
+  
+}
+
+
+
---
+++ app.R
@@ -0,0 +1,92 @@
+pacman::p_load(tidyverse, polite, scales, ggimage, rvest, 
+               glue, extrafont, showtext, ggrepel, magick, 
+               ggforce, ggtext, lubridate, 
+               cowplot, patchwork, rlang)
+library(shiny)
+source("Helpers.R")
+# Define UI for application that draws a histogram
+ui <- fluidPage(
+    
+    # Application title
+    titlePanel("Create your own age plot"),
+    
+    # Sidebar with a slider input for number of bins 
+    sidebarLayout(
+        sidebarPanel(
+            textInput("team", "Team", "pec-zwolle"),
+            textInput("teamcode", "Teamcode", "1269"),
+            actionButton("myButton", "Scrape!"),
+           
+            textInput("rect","Rectangle 'peak age' color", "red"),
+            textInput("line","Line contract length color", "black"),
+            textInput("line2","Line contract length color", "black"),
+            radioButtons("alpha", "See contract lines?",
+                         c("Yes" = 1,
+                           "No" = 0)),
+            actionButton("go", "Click here to plot!")
+            
+        ),
+        
+        # Show a plot of the generated distribution
+        mainPanel(
+            tabsetPanel(type = "tabs",
+                        tabPanel("Instruction", 
+                                 h4("Instructions", align = "center"),
+                                 h5("Go to transfermarkt.com and search for your favourite team.", align = "left"),
+                                 h5("When you're on the page of your team, look at the URL. It looks something like this:", align = "left"),
+                                 h5(" "),
+                                 h5("https://www.transfermarkt.com/inter-mailand/startseite/verein/46/saison_id/2019", align = "left"),
+                                 h5(" "),
+                                 h5("There are two thing important: the club (inter-mailand) and the teamcode (46).", align = "left"),
+                                 h5("The other number is for the season, but we ignore that. Just copy the two values and paste them in the boxes on the left (including the minus sign (-)", align = "left"),
+                                 h5("Click on the 'scrape!' button and wait a little while."),
+                                 h5("A (new )table on this page will appear which means the data is scraped and ready to plot!"),
+                                 br(),
+                                 h5("eNext, enter some colours from here:"),
+                                 h5("http://sape.inf.usi.ch/quick-reference/ggplot2/colour"),
+                                 h5("Or enter a hex colour code and click on 'plot!'"),
+                                 br(),
+                                 h5("Choose white a a colour to have no rectangle for peak age"),
+                                 tableOutput("myTable")),
+                        tabPanel("Age Plot", 
+                                 plotOutput("scatplot")))
+        )
+    )
+)
+
+# Define server logic required to draw a histogram
+server <- function(input, output) {
+    url=  reactive({
+        glue("https://www.transfermarkt.com/{input$team}/leistungsdaten/verein/{input$teamcode}/reldata/%262019/plus/1")
+    })
+    
+    myData <- reactive({
+        input$myButton
+        data = isolate(TransfermarktShiny(
+            team_name = input$team, 
+            team_num = input$teamcode))
+    })
+    output$myTable <- renderTable(myData())
+    
+    output$scatplot = renderPlot({
+        if (input$go == 0)
+            return()
+        req(input$go)
+        color1 <- isolate(input$rect)
+        color2 <- isolate(input$line)
+        color3 <- isolate(input$line2)
+        teamname <- isolate(input$team)
+        alpha <- isolate(input$alpha)
+        isolate(ScatterShiny(data = myData(),
+                             color1 = color1,
+                             color2 = color2,
+                             color3= color3,
+                             teamname = teamname,
+                             alpha = alpha))
+        
+        
+    }, height = 400, width = 750 )
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
