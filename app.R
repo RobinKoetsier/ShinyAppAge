9217f77b76e27662035a65e289c51276b0c6d7be
RobinWilhelmus <45625446+RobinWilhelmus@users.noreply.github.com>
2020-06-20 18:05:22 +0200

    Update app.R

--- app.R
+++ app.R
@@ -36,22 +36,24 @@ ui <- fluidPage(
             textInput("teamcode", "Teamcode", "1269"),
             actionButton("myButton", "Scrape!"),
             
-            textInput("rect","Rectangle 'peak age' color", "red"),
+            textInput("rect","Rectangle 'peak age' color (white will make it disappear)", "red"),
             textInput("line","Time ate club color", "black"),
             textInput("line2","Line contract length color", "black"),
+            textInput("dot","Dot color", "black"),
+            textInput("name","Player name color", "blue"),
             radioButtons("alpha", "See lines?",
                          c("Both lines" = 3,
                            "Only time at club" = 2,
                            "Only contract length" = 1,
                            "No lines" = 0)),
-            actionButton("go", "Click here to plot!")
+            actionButton("go", "Plot!")
             
         ),
         
         # Show a plot of the generated distribution
         mainPanel(
             tabsetPanel(type = "tabs",
-                        tabPanel("Instruction", 
+                        tabPanel("Instructions", 
                                  h4("Instructions", align = "center"),
                                  h5("Go to transfermarkt.com and search for your favourite team.", align = "left"),
                                  h5("When you're on the page of your team, look at the URL. It looks something like this:", align = "left"),
@@ -61,13 +63,13 @@ ui <- fluidPage(
                                  h5("There are two thing important: the club (inter-mailand) and the teamcode (46).", align = "left"),
                                  h5("The other number is for the season, but we ignore that. Just copy the two values and paste them in the boxes on the left (including the minus sign (-)", align = "left"),
                                  h5("Click on the 'scrape!' button and wait a little while."),
-                                 h5("A (new )table on this page will appear which means the data is scraped and ready to plot!"),
+                                 h5("A (new) table on this page will appear which means the data is scraped and ready to plot!"),
                                  br(),
-                                 h5("Next, enter some colours from here:"),
+                                 h5("Next, choose some colours from here:"),
                                  h5("http://sape.inf.usi.ch/quick-reference/ggplot2/colour"),
-                                 h5("Or enter a hex colour code and click on 'plot!'"),
+                                 h5("Or a hex colour code, enter them in the text fields and click on 'plot!'"),
                                  br(),
-                                 h5("Choose white a a colour to have no rectangle for peak age"),
+                                 h5("Choose white as a colour to have no rectangle for peak age"),
                                  tableOutput("myTable")),
                         tabPanel("Age Plot", 
                                  h5("Data scraped for:"),
@@ -83,13 +85,15 @@ server <- function(input, output) {
         glue("https://www.transfermarkt.com/{input$team}/leistungsdaten/verein/{input$teamcode}/reldata/%262019/plus/1")
         
     })
-    output$text1 <- renderText(url())
+    #output$text1 <- renderText(url())
+    
     myData <- reactive({
         input$myButton
         data = isolate(TransfermarktShiny(
             team_name = input$team, 
             team_num = input$teamcode))
     })
+    output$text1 <- renderText(myData()$Club[1])
     output$myTable <- renderTable(myData())
     
     output$scatplot = renderPlot({
